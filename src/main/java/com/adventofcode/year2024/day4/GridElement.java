8321b7aea57785c8e587be5e3f6e509a68eaa9c8
Ben Ferenchak <ben.ferenchak@gmail.com>
2025-11-25 16:08:37 -0500

    feat: modify GridElement files, and update CI workflow configuration.

--- src/main/java/com/adventofcode/year2024/day4/GridElement.java
+++ src/main/java/com/adventofcode/year2024/day4/GridElement.java
@@ -1,12 +1,10 @@
 package com.adventofcode.year2024.day4;
 
-
 import com.adventofcode.common.grid.ConstructableGridElement;
-import com.adventofcode.common.grid.PrintableGridElement;
 import lombok.Getter;
 
 @Getter
-public enum GridElement implements PrintableGridElement, ConstructableGridElement<GridElement> {
+public enum GridElement implements ConstructableGridElement<GridElement> {
 
     X("X"), M("M"), A("A"), S("S"), SPACE(" ");
 
